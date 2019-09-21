f18e5344aff4a20322aed75533e265e3285ed8c7
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-21 12:13:39 +0300

    created the components and the routes Relates #1 #2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { Shop } from './components/Shop';
+import { NotFound } from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/About" component={About} />
+          <Route path="/Shop" component={Shop} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const NotFound = () => {
+  return <h1>This Page Is Not Found</h1>;
+};
